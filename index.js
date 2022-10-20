a24f655a327625b8e2ccafc125a70f834c0106a9
Duni03 <61987129+Duni03@users.noreply.github.com>
2022-10-20 15:45:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf("5738959061:AAGwOUxq4k-QKhf11UU5nO3XuaAslwCQ2pU")
+
+/// are standard commands
+
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+
+// custom commands
+
+bot.command("nihal", (ctx) => {
+  ctx.reply("do not disturb");
+})
+
+// repiies after receiving a sticker (event)
+
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+
+// hears is used for text rather than tags
+
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+exports.handler = (event,context,callback) => {
+  const tmp = JSON.parse(event.body);
+  bot.handleUpdate(tmp);
+  return callback(null, {
+    statusCode: 200,
+    body: '',
+  })
+}
